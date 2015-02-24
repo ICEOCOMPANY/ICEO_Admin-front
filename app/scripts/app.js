65be8c23b6a0356bb4b9c22c153f4c92d18ee77e
Kamil Gruszowski <k.gruszowski@iceo.co>
2015-02-24 16:00:40 +0100

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,150 @@
+/* Main App */
+
+var app = angular.module('ICEOapp', [
+    'ngStorage',
+    'ui.router',
+    'ngRoute',
+    'angularFileUpload',
+    'oc.lazyLoad'
+]);
+app.config(['$urlRouterProvider', '$httpProvider', '$stateProvider', 'ASSETS', function ($urlRouterProvider, $httpProvider, $stateProvider, ASSETS) {
+
+        //Router managing paths (input controller)
+        /*$routeProvider.
+         when('/', {
+         templateUrl: 'partials/home.html'
+         }).
+         when('/signin', {
+         templateUrl: 'partials/signin.html'
+         }).
+         when('/me', {
+         templateUrl: 'partials/profile.html'
+         }).
+         when('/remind', {
+         templateUrl: 'partials/remind.html'
+         }).
+         when('/upload', {
+         templateUrl: 'partials/upload.html',
+         controller: 'FileCtrl'
+         }).
+         otherwise({
+         redirectTo: '/'
+         });*/
+
+        $urlRouterProvider.otherwise("/admin/dashboard");
+
+        $stateProvider.
+                state('login', {
+                    url: '/login',
+                    templateUrl: appHelper.templatePath('auth'),
+                    resolve: {
+                        resources: function ($ocLazyLoad) {
+                            return $ocLazyLoad.load([
+                                ASSETS.forms.jQueryValidate
+                            ]);
+                        }
+                    }
+                }).
+                state('admin', {
+                    abstract: true,
+                    url: '/admin',
+                    templateUrl: appHelper.templatePath('admin/app-body'),
+                    controller: function ($rootScope) {
+                        $rootScope.isLoginPage = false;
+                        $rootScope.isLightLoginPage = false;
+                        $rootScope.isLockscreenPage = false;
+                        $rootScope.isMainPage = true;
+                    }
+                }).state('admin.dashboard', {
+                    url: '/dashboard',
+                    templateUrl: appHelper.templatePath('admin/dashboard'),
+                    resolve: {
+                        resources: function ($ocLazyLoad) {
+                            return $ocLazyLoad.load([
+                                ASSETS.forms.jQueryValidate
+                            ]);
+                        }
+                    }
+                });
+                
+                
+        $httpProvider.interceptors.push(['$rootScope', '$q', '$location', '$localStorage', function ($rootScope, $q, $location, $localStorage) {
+                return {
+                    'request': function (config) {
+                        //Inject Authorization header to recognition user
+                        config.headers = config.headers || {};
+                        if ($localStorage.token) {
+                            config.headers.Authorization = $localStorage.token;
+                        }
+                        return config;
+                    },
+                    'responseError': function (response) {
+                        //If request contains extinct token -> clean $localStorage, token, and redirect to sign in page with error message
+                        if (response.status === 401 || response.status === 403) {
+                            delete $localStorage.token;
+                            $rootScope.token = null;
+                            $rootScope.error = "Twoja sesja wygasła! zaloguj się ponownie.";
+                            $location.path('/signin');
+                        }
+                        return $q.reject(response);
+                    }
+                };
+            }]);
+    }
+]);
+
+app.constant('ASSETS', {
+    'core': {
+        'bootstrap': appHelper.assetPath('js/bootstrap.min.js'), // Some plugins which do not support angular needs this
+
+        'jQueryUI': [
+            appHelper.assetPath('js/jquery-ui/jquery-ui.min.js'),
+            appHelper.assetPath('js/jquery-ui/jquery-ui.structure.min.css')
+        ],
+        'moment': appHelper.assetPath('js/moment.min.js'),
+        'googleMapsLoader': appHelper.assetPath('app/js/angular-google-maps/load-google-maps.js')
+    },
+    'icons': {
+        'meteocons': appHelper.assetPath('css/fonts/meteocons/css/meteocons.css'),
+        'elusive': appHelper.assetPath('css/fonts/elusive/css/elusive.css')
+    },
+    'forms': {
+        'select2': [
+            appHelper.assetPath('js/select2/select2.css'),
+            appHelper.assetPath('js/select2/select2-bootstrap.css'),
+            appHelper.assetPath('js/select2/select2.min.js')
+        ],
+        'daterangepicker': [
+            appHelper.assetPath('js/daterangepicker/daterangepicker-bs3.css'),
+            appHelper.assetPath('js/daterangepicker/daterangepicker.js')
+        ],
+        'colorpicker': appHelper.assetPath('js/colorpicker/bootstrap-colorpicker.min.js'),
+        'selectboxit': appHelper.assetPath('js/selectboxit/jquery.selectBoxIt.js'),
+        'tagsinput': appHelper.assetPath('js/tagsinput/bootstrap-tagsinput.min.js'),
+        'datepicker': appHelper.assetPath('js/datepicker/bootstrap-datepicker.js'),
+        'timepicker': appHelper.assetPath('js/timepicker/bootstrap-timepicker.min.js'),
+        'inputmask': appHelper.assetPath('js/inputmask/jquery.inputmask.bundle.js'),
+        'formWizard': appHelper.assetPath('js/formwizard/jquery.bootstrap.wizard.min.js'),
+        'jQueryValidate': appHelper.assetPath('js/jquery-validate/jquery.validate.min.js'),
+        'dropzone': [
+            appHelper.assetPath('js/dropzone/css/dropzone.css'),
+            appHelper.assetPath('js/dropzone/dropzone.min.js')
+        ],
+        'typeahead': [
+            appHelper.assetPath('js/typeahead.bundle.js'),
+            appHelper.assetPath('js/handlebars.min.js')
+        ],
+        'multiSelect': [
+            appHelper.assetPath('js/multiselect/css/multi-select.css'),
+            appHelper.assetPath('js/multiselect/js/jquery.multi-select.js')
+        ],
+        'icheck': [
+            appHelper.assetPath('js/icheck/skins/all.css'),
+            appHelper.assetPath('js/icheck/icheck.min.js')
+        ],
+        'bootstrapWysihtml5': [
+            appHelper.assetPath('js/wysihtml5/src/bootstrap-wysihtml5.css'),
+            appHelper.assetPath('js/wysihtml5/wysihtml5-angular.js')
+        ]
+    }
+});
---
+++ app/scripts/services.js
@@ -0,0 +1,89 @@
+/* Services and Factories */
+
+var ICEOapp = angular.module('ICEOapp');
+
+/**
+ * Service to handle basic functions
+ */
+ICEOapp.factory('MainFactory', ['$http', '$localStorage', function ($http, $localStorage) {
+
+        //API domain url - here you should define a domain of your REST API!
+        var baseUrl = "http://localhost/Cloud";
+
+        function changeUser(user) {
+            angular.extend(currentUser, user);
+        }
+
+        function urlBase64Decode(str) {
+            var output = str.replace('-', '+').replace('_', '/');
+            switch (output.length % 4) {
+                case 0:
+                    break;
+                case 2:
+                    output += '==';
+                    break;
+                case 3:
+                    output += '=';
+                    break;
+                default:
+                    throw 'Illegal base64url string!';
+            }
+            return window.atob(output);
+        }
+
+        function getUserFromToken() {
+            var token = $localStorage.token;
+            var user = {};
+            if (typeof token !== 'undefined') {
+                var encoded = token.split('.')[1];
+                user = JSON.parse(urlBase64Decode(encoded));
+            }
+            return user;
+        }
+
+        var currentUser = getUserFromToken();
+
+        //set Content-Type to prevent browser from send preflight OPTIONS to domain
+        $http.defaults.headers.post["Content-Type"] = 'text/plain';
+        $http.defaults.headers.put["Content-Type"] = 'text/plain';
+
+        return {
+            profile: function(success, error){
+                $http.get(baseUrl + '/admin/auth').success(success).error(error)
+            },
+            signin: function (data, success, error) {
+                $http.post(baseUrl + '/admin/auth', data).success(success).error(error)
+            },
+            logout: function (success, error) {
+                $http.delete(baseUrl + '/admin/auth').success(success).error(error)
+                changeUser({});
+            },
+            remind: function(data, success, error){
+                $http.post(baseUrl+"/users/password", data).success(success).error(error)
+            },
+            reset: function(data, success, error){
+                $http.put(baseUrl+"/users/password", data).success(success).error(error)
+            },
+            activate: function(data, success, error){
+                $http.post(baseUrl+"/users/activation", data).success(success).error(error)
+            }
+        };
+    }
+    ]);
+
+ICEOapp.factory('TokenFactory', ['$http', '$localStorage', function ($http, $localStorage) {
+
+    var token;
+
+    return{
+        setToken: function(value){
+            token = value;
+            console.log("Ustawiam token na"+token)
+        },
+        getToken: function(){
+            console.log("Zwracam token na"+token)
+            return token;
+        }
+    }
+
+}]);
